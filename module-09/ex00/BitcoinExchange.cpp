04b7d3a89e29a87abf34c648fafe46d92573ca34
Arsen Badalyan <arsenbadalyan.01@gmail.com>
2023-12-09 14:46:55 +0400

    fix: module-09: bugfixes, space indentation to tabs

--- module-09/ex00/BitcoinExchange.cpp
+++ module-09/ex00/BitcoinExchange.cpp
@@ -14,240 +14,240 @@ BitcoinExchange::~BitcoinExchange() {}
 void BitcoinExchange::operator=( const BitcoinExchange & ) {}
 
 void BitcoinExchange::validateDB( void ) {
-    std::ifstream DB;
+	std::ifstream DB;
 
-    DB.open(BitcoinExchange::DB_NAME);
+	DB.open(BitcoinExchange::DB_NAME);
 
-    if (!DB)
-        throw std::runtime_error(ERR_UNAVAILABLE_DB);
+	if (!DB)
+		throw std::runtime_error(ERR_UNAVAILABLE_DB);
 
-    std::string fileContent;
-    std::string btcCourseKey, btcCourseValue;
-    size_t commaPos;
-    bool isEmpty = true;
+	std::string fileContent;
+	std::string btcCourseKey, btcCourseValue;
+	size_t commaPos;
+	bool isEmpty = true;
 
-    DB >> fileContent;
-    commaPos = fileContent.find(',');
-    if (commaPos == std::string::npos
-        || fileContent.substr(0, commaPos) != BitcoinExchange::DB_DATE_COL_NAME
-        || fileContent.substr(commaPos + 1, fileContent.length()) != BitcoinExchange::DB_EXCHANGE_COL_NAME)
-            throw std::runtime_error(ERR_INVALID_DB);
+	DB >> fileContent;
+	commaPos = fileContent.find(',');
+	if (commaPos == std::string::npos
+		|| fileContent.substr(0, commaPos) != BitcoinExchange::DB_DATE_COL_NAME
+		|| fileContent.substr(commaPos + 1, fileContent.length()) != BitcoinExchange::DB_EXCHANGE_COL_NAME)
+			throw std::runtime_error(ERR_INVALID_DB);
 
-    while(DB.good()) {
-        if (isEmpty)
-            isEmpty = false;
-        DB >> fileContent;
+	while(DB.good()) {
+		if (isEmpty)
+			isEmpty = false;
+		DB >> fileContent;
 
-        commaPos = fileContent.find(',');
+		commaPos = fileContent.find(',');
 
-        if (commaPos == std::string::npos)
-            throw std::runtime_error(ERR_INVALID_DB);
+		if (commaPos == std::string::npos)
+			throw std::runtime_error(ERR_INVALID_DB);
 
-        btcCourseKey = fileContent.substr(0, commaPos);
-        btcCourseValue = fileContent.substr(commaPos + 1, fileContent.length());
-        if (BitcoinExchange::isValidDate(btcCourseKey)
-            && BitcoinExchange::isValidAndInBoundsNumber(btcCourseValue, true))
-                BitcoinExchange::DB_COURSES_MAP[btcCourseKey] = std::strtod(btcCourseValue.c_str(), NULL);
-        else {
-            BitcoinExchange::DB_COURSES_MAP.clear();
-            throw std::runtime_error(ERR_INVALID_DB);
-        }
-    }
+		btcCourseKey = fileContent.substr(0, commaPos);
+		btcCourseValue = fileContent.substr(commaPos + 1, fileContent.length());
+		if (BitcoinExchange::isValidDate(btcCourseKey)
+			&& BitcoinExchange::isValidAndInBoundsNumber(btcCourseValue, true))
+				BitcoinExchange::DB_COURSES_MAP[btcCourseKey] = std::strtod(btcCourseValue.c_str(), NULL);
+		else {
+			BitcoinExchange::DB_COURSES_MAP.clear();
+			throw std::runtime_error(ERR_INVALID_DB);
+		}
+	}
 
-    if (isEmpty || BitcoinExchange::DB_COURSES_MAP.begin() == BitcoinExchange::DB_COURSES_MAP.end())
-        throw std::runtime_error(ERR_EMPTY_DB);
+	if (isEmpty || BitcoinExchange::DB_COURSES_MAP.begin() == BitcoinExchange::DB_COURSES_MAP.end())
+		throw std::runtime_error(ERR_EMPTY_DB);
 }
 
 void BitcoinExchange::executeInput( char * inputFile ) {
-    const std::string fileName(inputFile);
-    std::ifstream fileReader(fileName);
+	const std::string fileName(inputFile);
+	std::ifstream fileReader(fileName);
 
-    if (!fileReader)
-        throw std::runtime_error(ERR_INVALID_INPUT_FILE);
+	if (!fileReader)
+		throw std::runtime_error(ERR_INVALID_INPUT_FILE);
 
-    std::string fileContent, date, course;
-    string_vector inputLine;
-    string_vector::iterator inputLineIt;
-    char delimiter = '|';
+	std::string fileContent, date, course;
+	string_vector inputLine;
+	string_vector::iterator inputLineIt;
+	char delimiter = '|';
 
-    std::getline(fileReader, fileContent);
-    inputLine = BitcoinExchange::split(fileContent, delimiter);
-    inputLineIt = inputLine.begin();
+	std::getline(fileReader, fileContent);
+	inputLine = BitcoinExchange::split(fileContent, delimiter);
+	inputLineIt = inputLine.begin();
 
-    if ((std::distance(inputLineIt, inputLine.end()) == 2)
-        && BitcoinExchange::trimWhitespaces(*inputLineIt) == BitcoinExchange::INPUT_FIRST_COL_NAME
-        && BitcoinExchange::trimWhitespaces(*(++inputLineIt)) == BitcoinExchange::INPUT_SECOND_COL_NAME)
-        std::getline(fileReader, fileContent);
+	if ((std::distance(inputLineIt, inputLine.end()) == 2)
+		&& BitcoinExchange::trimWhitespaces(*inputLineIt) == BitcoinExchange::INPUT_FIRST_COL_NAME
+		&& BitcoinExchange::trimWhitespaces(*(++inputLineIt)) == BitcoinExchange::INPUT_SECOND_COL_NAME)
+		std::getline(fileReader, fileContent);
 
-    while (fileReader) {
+	while (fileReader) {
 
-        if (!BitcoinExchange::trimWhitespaces(fileContent).length()) {
-            std::getline(fileReader, fileContent);
-            continue ;
-        }
+		if (!BitcoinExchange::trimWhitespaces(fileContent).length()) {
+			std::getline(fileReader, fileContent);
+			continue ;
+		}
 
-        inputLine = BitcoinExchange::split(fileContent, delimiter);
-        inputLineIt = inputLine.begin();
+		inputLine = BitcoinExchange::split(fileContent, delimiter);
+		inputLineIt = inputLine.begin();
 
-        try {
-            if (std::distance(inputLineIt, inputLine.end()) != 2)
-                throw std::runtime_error(ERR_BAD_INPUT + fileContent);
+		try {
+			if (std::distance(inputLineIt, inputLine.end()) != 2)
+				throw std::runtime_error(ERR_BAD_INPUT + fileContent);
 
-            date = BitcoinExchange::trimWhitespaces(*inputLineIt);
-            course = BitcoinExchange::trimWhitespaces(*(++inputLineIt));
+			date = BitcoinExchange::trimWhitespaces(*inputLineIt);
+			course = BitcoinExchange::trimWhitespaces(*(++inputLineIt));
 
-            if (!BitcoinExchange::isValidDate(date))
-                throw std::runtime_error(ERR_BAD_INPUT + fileContent);
+			if (!BitcoinExchange::isValidDate(date)
+				|| !BitcoinExchange::isValidAndInBoundsNumber(course, false))
+				throw std::runtime_error(ERR_BAD_INPUT + fileContent);
 
-            BitcoinExchange::isValidAndInBoundsNumber(course, false);
-            BitcoinExchange::calculateCourse(date, course);
-        } catch (const std::exception & exc) {
-            std::cout << "Error: " << exc.what() << std::endl;
-        }
+			BitcoinExchange::calculateCourse(date, course);
+		} catch (const std::exception & exc) {
+			std::cout << "Error: " << exc.what() << std::endl;
+		}
 
-        std::getline(fileReader, fileContent);
-    }
+		std::getline(fileReader, fileContent);
+	}
 }
 
 void BitcoinExchange::calculateCourse( const std::string & date, const std::string & course ) {
-    db_map::iterator target;
-    db_map::iterator db_start = BitcoinExchange::DB_COURSES_MAP.begin();
-    db_map::iterator near_bound = BitcoinExchange::DB_COURSES_MAP.lower_bound(date);
+	db_map::iterator target;
+	db_map::iterator db_start = BitcoinExchange::DB_COURSES_MAP.begin();
+	db_map::iterator near_bound = BitcoinExchange::DB_COURSES_MAP.lower_bound(date);
 
-    target = near_bound;
-    if (near_bound != db_start)
-        target = --near_bound;
-    
-    const double result = std::strtod(course.c_str(), NULL) * target->second;
+	target = near_bound;
+	if (near_bound != db_start)
+		target = --near_bound;
+	
+	const double result = std::strtod(course.c_str(), NULL) * target->second;
 
-    std::cout << date << " => " << course << " = " << result << std::endl;
+	std::cout << date << " => " << course << " = " << result << std::endl;
 }
 
 bool BitcoinExchange::isValidDate( const std::string & date ) {
-    string_vector datePart;
-    string_vector::iterator dateIt;
-    int month;
-    int year;
+	string_vector datePart;
+	string_vector::iterator dateIt;
+	int month;
+	int year;
 
-    datePart = BitcoinExchange::split(date, '-');
-    dateIt = datePart.begin();
+	datePart = BitcoinExchange::split(date, '-');
+	dateIt = datePart.begin();
 
-    if (std::distance(dateIt, datePart.end()) != 3)
-        return (false);
+	if (std::distance(dateIt, datePart.end()) != 3)
+		return (false);
 
-    // DATE YEAR VALIDATION
-    if (!(*dateIt).length() || (*dateIt).length() > 4
-        || !BitcoinExchange::isOnlyDigit((*dateIt), false)) {
-        return (false);
-    }
-    year = std::atoi((*dateIt).c_str());
+	// DATE YEAR VALIDATION
+	if (!(*dateIt).length() || (*dateIt).length() > 4
+		|| !BitcoinExchange::isOnlyDigit((*dateIt), false)) {
+		return (false);
+	}
+	year = std::atoi((*dateIt).c_str());
 
-    dateIt++;
+	dateIt++;
 
-    // DATE MONTH VALIDATION
-    if ((*dateIt).length() && (*dateIt).length() <= 2
-        && BitcoinExchange::isOnlyDigit((*dateIt), false)) {
-            month = std::atoi((*dateIt).c_str());
+	// DATE MONTH VALIDATION
+	if ((*dateIt).length() && (*dateIt).length() <= 2
+		&& BitcoinExchange::isOnlyDigit((*dateIt), false)) {
+			month = std::atoi((*dateIt).c_str());
 
-            if (month < 1 || month > 12)
-                return (false);
-        } else return (false);
+			if (month < 1 || month > 12)
+				return (false);
+		} else return (false);
 
-    dateIt++;
+	dateIt++;
 
-    // DATE DAY VALIDATION
-    if ((*dateIt).length() && (*dateIt).length() <= 2
-        && BitcoinExchange::isOnlyDigit((*dateIt), false)) {
-            int day = std::atoi((*dateIt).c_str());
-            int dayLimit = BitcoinExchange::MONTH_MAX_DAY[month - 1];
-            if (month == 2 && BitcoinExchange::isLeapYear(year))
-                dayLimit++;
+	// DATE DAY VALIDATION
+	if ((*dateIt).length() && (*dateIt).length() <= 2
+		&& BitcoinExchange::isOnlyDigit((*dateIt), false)) {
+			int day = std::atoi((*dateIt).c_str());
+			int dayLimit = BitcoinExchange::MONTH_MAX_DAY[month - 1];
+			if (month == 2 && BitcoinExchange::isLeapYear(year))
+				dayLimit++;
 
-            if (day > dayLimit)
-                return (false);
+			if (day > dayLimit)
+				return (false);
 
-        } else return (false);
+		} else return (false);
 
-    return (true);
+	return (true);
 }
 
 string_vector BitcoinExchange::split( const std::string & str, char delimiter ) {
-    string_vector result;
-    size_t start = 0;
-    size_t end = str.find(delimiter);
+	string_vector result;
+	size_t start = 0;
+	size_t end = str.find(delimiter);
 
-    while (end != std::string::npos) {
-        result.push_back(str.substr(start, end - start));
-        start = end + 1;
-        end = str.find(delimiter, start);
-    }
+	while (end != std::string::npos) {
+		result.push_back(str.substr(start, end - start));
+		start = end + 1;
+		end = str.find(delimiter, start);
+	}
 
-    result.push_back(str.substr(start));
-    return result;
+	result.push_back(str.substr(start));
+	return result;
 }
 
 std::string BitcoinExchange::trimWhitespaces( std::string & str ) {
-    std::string::iterator start = str.begin();
-    std::string::iterator end = str.end();
+	std::string::iterator start = str.begin();
+	std::string::iterator end = str.end();
 
-    while (start != end && std::isspace(*start))
-        start++;
+	while (start != end && std::isspace(*start))
+		start++;
 
-    if (start == end)
-        return (std::string());
+	if (start == end)
+		return (std::string());
 
-    end--;
+	end--;
 
-    while (std::isspace(*end))
-        end--;
+	while (std::isspace(*end))
+		end--;
 
-    return std::string(start, end + 1);
+	return std::string(start, end + 1);
 }
 
 bool BitcoinExchange::isOnlyDigit( const std::string & str, bool checkDot ) {
-    size_t idx = 0;
-    bool hasAlreadyDot = false;
+	size_t idx = 0;
+	bool hasAlreadyDot = false;
 
-    if (str[idx] && (str[idx] == '+' || str[idx] == '-') && str[idx + 1])
-        idx++;
+	if (str[idx] && (str[idx] == '+' || str[idx] == '-') && str[idx + 1])
+		idx++;
 
-    while (str[idx]) {
-        if (!std::isdigit(str[idx])) {
-            if (checkDot && !hasAlreadyDot
-                && str[idx] == '.' && idx != 0
-                && idx != str.length() - 1) {
-                hasAlreadyDot = true;
-                idx++;
-                continue ;
-            }
-            return (false);
-        }
-        idx++;
-    }
+	while (str[idx]) {
+		if (!std::isdigit(str[idx])) {
+			if (checkDot && !hasAlreadyDot
+				&& str[idx] == '.' && idx != 0
+				&& idx != str.length() - 1) {
+				hasAlreadyDot = true;
+				idx++;
+				continue ;
+			}
+			return (false);
+		}
+		idx++;
+	}
 
-    return (true);
+	return (true);
 }
 
 bool BitcoinExchange::isLeapYear( int year ) {
-    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
 bool BitcoinExchange::isValidAndInBoundsNumber( const std::string & source, bool isDBCol ) {
-    if (BitcoinExchange::isOnlyDigit(source, true)) {
-        double num = std::strtod(source.c_str(), NULL);
+	if (BitcoinExchange::isOnlyDigit(source, true)) {
+		double num = std::strtod(source.c_str(), NULL);
 
-        if (isDBCol && num >= 0) 
-            return (true);
+		if (isDBCol && num >= 0) 
+			return (true);
 
-        if (!isDBCol) {
-            if (num >= BitcoinExchange::MIN_BOUND && num <= BitcoinExchange::MAX_BOUND)
-                return (true);
-            else if (num < BitcoinExchange::MIN_BOUND)
-                throw std::runtime_error(ERR_NEG_NUM);
-            else if (num > BitcoinExchange::MAX_BOUND)
-                throw std::runtime_error(ERR_LARGE_NUM);
-        }
-    }
+		if (!isDBCol) {
+			if (num >= BitcoinExchange::MIN_BOUND && num <= BitcoinExchange::MAX_BOUND)
+				return (true);
+			else if (num < BitcoinExchange::MIN_BOUND)
+				throw std::runtime_error(ERR_NEG_NUM);
+			else if (num > BitcoinExchange::MAX_BOUND)
+				throw std::runtime_error(ERR_LARGE_NUM);
+		}
+	}
 
-    return (false);
+	return (false);
 }
--- module-09/ex01/RPN.cpp
+++ module-09/ex01/RPN.cpp
@@ -13,97 +13,99 @@ RPN::~RPN() {}
 void RPN::operator=( const RPN & ) {}
 
 void RPN::executeExpression( char * expression ) {
-    if (!expression)
-        throw std::runtime_error(ERR_WRONG_ARG_LIST);
+	if (!expression)
+		throw std::runtime_error(ERR_WRONG_ARG_LIST);
 
-    const std::string expr(expression);
-    const std::string *exprList;
-    size_t expressionListSize = 0;
+	const std::string expr(expression);
+	const std::string *exprList;
+	size_t expressionListSize = 0;
 
-    exprList = RPN::split(expr, ' ', expressionListSize);
-    
-    try {
-        for (size_t i = 0; i < expressionListSize; i++) {
-            if (!isValidExpressionItem(exprList[i]))
-                std::runtime_error("");
-        }
-    } catch (...) {
-        delete[] exprList;
-        throw std::runtime_error(ERR_WRONG_EXPR);
-    }
+	exprList = RPN::split(expr, ' ', expressionListSize);
 
-    std::stack<double> expressionStack;
-    double rvalue, lvalue;
-    ExecuteOperation operations[4] = { &RPN::plus, &RPN::minus, &RPN::multiply, &RPN::divide };
+	try {
+		if (!expressionListSize)
+			throw std::runtime_error("");
+		for (size_t i = 0; i < expressionListSize; i++) {
+			if (!isValidExpressionItem(exprList[i]))
+				throw std::runtime_error("");
+		}
+	} catch (...) {
+		delete[] exprList;
+		throw std::runtime_error(ERR_WRONG_EXPR);
+	}
 
-    try {
-        for (size_t i = 0; i < expressionListSize; i++) {
-            char expressionItem = exprList[i][0];
-            size_t operationIndex = RPN::ACCEPTED_NUMERIC_OPERATIONS.find(expressionItem);
+	std::stack<double> expressionStack;
+	double rvalue, lvalue;
+	ExecuteOperation operations[4] = { &RPN::plus, &RPN::minus, &RPN::multiply, &RPN::divide };
 
-            if (operationIndex == std::string::npos) {
-                expressionStack.push(std::strtod(exprList[i].c_str(), NULL));
-            } else {
-                if (expressionStack.size() < 2)
-                    throw std::runtime_error(ERR_WRONG_EXPR);
+	try {
+		for (size_t i = 0; i < expressionListSize; i++) {
+			char expressionItem = exprList[i][0];
+			size_t operationIndex = RPN::ACCEPTED_NUMERIC_OPERATIONS.find(expressionItem);
 
-                rvalue = expressionStack.top();
-                expressionStack.pop();
-                lvalue = expressionStack.top();
-                expressionStack.pop();
+			if (operationIndex == std::string::npos) {
+				expressionStack.push(std::strtod(exprList[i].c_str(), NULL));
+			} else {
+				if (expressionStack.size() < 2)
+					throw std::runtime_error(ERR_WRONG_EXPR);
 
-                expressionStack.push(operations[operationIndex](rvalue, lvalue));
-            }
-        }
+				rvalue = expressionStack.top();
+				expressionStack.pop();
+				lvalue = expressionStack.top();
+				expressionStack.pop();
 
-        double result = expressionStack.top();
-        expressionStack.pop();
+				expressionStack.push(operations[operationIndex](rvalue, lvalue));
+			}
+		}
 
-        if (expressionStack.size())
-            throw std::runtime_error(ERR_WRONG_EXPR);
+		double result = expressionStack.top();
+		expressionStack.pop();
 
-        std::cout << "Result: " << result << std::endl;
-    } catch(const std::exception* exc) {
-        throw std::runtime_error(exc->what());
-    }
+		if (expressionStack.size())
+			throw std::runtime_error(ERR_WRONG_EXPR);
 
-    delete[] exprList;
+		std::cout << "Result: " << result << std::endl;
+	} catch(const std::exception* exc) {
+		throw std::runtime_error(exc->what());
+	}
+
+	delete[] exprList;
 }
 
 bool RPN::isValidExpressionItem( const std::string & exprItem ) {
-    if (!exprItem.length() || exprItem.length() > 1)
-        return (false);
+	if (!exprItem.length() || exprItem.length() > 1)
+		return (false);
 
-    if (!::isdigit(exprItem[0]) || ACCEPTED_NUMERIC_OPERATIONS.find(exprItem[0]) == std::string::npos)
-        return (false);
+	if (!::isdigit(exprItem[0]) && ACCEPTED_NUMERIC_OPERATIONS.find(exprItem[0]) == std::string::npos)
+		return (false);
 
-    return (true);
+	return (true);
 }
 
 std::string* RPN::split(const std::string& str, char delimiter, size_t& size) {
-    size = 0;
+	size = 0;
 
-    std::string token;
-    std::stringstream ss(str);
+	std::string token;
+	std::stringstream ss(str);
 
-    while (std::getline(ss, token, delimiter)) {
-        // Davs brats vonc paymanavorvel enq
-        if(!token.empty())
-            size++;
-    }
+	while (std::getline(ss, token, delimiter)) {
+		// Davs brats vonc paymanavorvel enq
+		if(!token.empty())
+			size++;
+	}
 
-    std::string *dividedList = new std::string[size];
-    size_t fillSize = 0;
+	std::string *dividedList = new std::string[size];
+	size_t fillSize = 0;
 
-    ss.clear();
-    ss.seekg(0);
+	ss.clear();
+	ss.seekg(0);
 
-    while (std::getline(ss, token, delimiter)) {
-        if(!token.empty()) {
-            dividedList[fillSize] = token;
-            fillSize++;
-        }
-    }
+	while (std::getline(ss, token, delimiter)) {
+		if(!token.empty()) {
+			dividedList[fillSize] = token;
+			fillSize++;
+		}
+	}
 
-    return (dividedList);
+	return (dividedList);
 }
